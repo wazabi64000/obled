2a47a3fc65bac15d7a479aaefe9af2cdd679e9ab
wazabi64000 <wazabi64000@gmail.com>
2025-08-20 01:37:31 +0200

    api

--- react-auth-client/src/services/api.js
+++ react-auth-client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/auth",
+  baseURL: import.meta.env.VITE_API_URL || "https://obledapi.onrender.com/api/auth",
   withCredentials: true,
  
  
