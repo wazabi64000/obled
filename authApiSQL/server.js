793d1f3dc1420a94fb7c4bbf080398452c69a185
wazabi64000 <wazabi64000@gmail.com>
2025-08-28 15:52:23 +0200

    database

--- authApiSQL/server.js
+++ authApiSQL/server.js
@@ -46,7 +46,7 @@ app.use(cors({
 
 app.use('/api/auth',limiter, authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
